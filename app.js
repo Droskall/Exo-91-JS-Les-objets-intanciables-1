3072a003dbb12e68b34897d35462c95e4b0d23af
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-02 09:07:10 +0100

    finish

---
+++ app.js
@@ -0,0 +1,43 @@
+let Personne = function (nom, prenom, age, sex) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.sex = sex;
+
+    this.getIdentity = function () {
+        return this.nom + " " + this.prenom + " a " + this.age + " et est un/une " + this.sex;
+    }
+
+    this.getChange = function (nom, prenom) {
+        return (this.nom = nom) && (this.prenom = prenom);
+    }
+
+}
+
+let john = new Personne("Doe", "John", "35 ans", "male");
+let dada = new Personne("Olivier", "Dada", "21 ans", "male");
+
+let propriete1 = document.getElementsByClassName("propriet1");
+let propriete2 = document.getElementsByClassName("propriet2");
+let propriete3 = document.getElementsByClassName("propriet3");
+let propriete4 = document.getElementsByClassName("propriet4");
+let method1 = document.getElementsByClassName("method1");
+let method2 = document.getElementsByClassName("method2");
+
+propriete1[0].innerHTML = john.nom;
+propriete2[0].innerHTML = john.prenom;
+propriete3[0].innerHTML = john.age;
+propriete4[0].innerHTML = john.sex;
+
+propriete1[1].innerHTML = steve.nom;
+propriete2[1].innerHTML = steve.prenom;
+propriete3[1].innerHTML = steve.age;
+propriete4[1].innerHTML = steve.sex;
+
+method1[0].innerHTML = john.getIdentity();
+john.getChange("John", "Doe");
+method2[0].innerHTML = john.getIdentity();
+
+method1[1].innerHTML = steve.getIdentity();
+dada.getChange("Olivier", "Damien");
+method2[1].innerHTML = steve.getIdentity();
